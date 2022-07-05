6662b468afa55669d6c2cce2a179ab076fab99ce
megmil <megmil@umich.edu>
2022-07-05 14:16:46 -0700

    Added roomVC views

---
+++ Soundroom/ViewControllers/RoomViewController.m
@@ -0,0 +1,20 @@
+//
+//  RoomViewController.m
+//  Soundroom
+//
+//  Created by Megan Miller on 7/5/22.
+//
+
+#import "RoomViewController.h"
+
+@interface RoomViewController ()
+
+@end
+
+@implementation RoomViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+@end
---
+++ Soundroom/Views/RoomCell.m
@@ -0,0 +1,23 @@
+//
+//  RoomCell.m
+//  Soundroom
+//
+//  Created by Megan Miller on 7/5/22.
+//
+
+#import "RoomCell.h"
+
+@implementation RoomCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
