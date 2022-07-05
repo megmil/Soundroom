c96c280130c34b95c0cfc08ff1e55457e1bd4450
megmil <megmil@umich.edu>
2022-07-05 12:59:34 -0700

    Added profileVC views

---
+++ Soundroom/ViewControllers/ProfileViewController.h
@@ -0,0 +1,16 @@
+//
+//  ProfileViewController.h
+//  Soundroom
+//
+//  Created by Megan Miller on 7/5/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ProfileViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Soundroom/Views/RecapCell.m
@@ -0,0 +1,23 @@
+//
+//  RecapCell.m
+//  Soundroom
+//
+//  Created by Megan Miller on 7/5/22.
+//
+
+#import "RecapCell.h"
+
+@implementation RecapCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
