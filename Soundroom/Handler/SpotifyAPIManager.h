78fbfe212dac0cb892597a52b420bec4d128a035
megmil <megmil@umich.edu>
2022-07-06 10:17:20 -0700

    Created files for API manager

---
+++ Soundroom/Handler/SpotifyAPIManager.h
@@ -0,0 +1,16 @@
+//
+//  SpotifyAPIManager.h
+//  Soundroom
+//
+//  Created by Megan Miller on 7/6/22.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SpotifyAPIManager
+
+@end
+
+NS_ASSUME_NONNULL_END
