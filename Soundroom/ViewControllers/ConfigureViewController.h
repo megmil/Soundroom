27fad81e8eb5bf7c3523e57b58e35e38fe24d10f
megmil <megmil@umich.edu>
2022-07-11 15:03:47 -0700

    Added VCs and buttons

---
+++ Soundroom/ViewControllers/ConfigureViewController.h
@@ -0,0 +1,16 @@
+//
+//  ConfigureViewController.h
+//  Soundroom
+//
+//  Created by Megan Miller on 7/11/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ConfigureViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Soundroom/ViewControllers/LobbyViewController.m
@@ -0,0 +1,20 @@
+//
+//  LobbyViewController.m
+//  Soundroom
+//
+//  Created by Megan Miller on 7/11/22.
+//
+
+#import "LobbyViewController.h"
+
+@interface LobbyViewController ()
+
+@end
+
+@implementation LobbyViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+@end
