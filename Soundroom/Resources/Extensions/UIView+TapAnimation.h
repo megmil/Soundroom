8227dddb97a3ebc77cf62f5b328d748d2c544c32
megmil <megmil@umich.edu>
2022-08-10 15:20:27 -0700

    Added TapAnimation category

---
+++ Soundroom/Resources/Extensions/UIView+TapAnimation.h
@@ -0,0 +1,18 @@
+//
+//  UIView+TapAnimation.h
+//  Soundroom
+//
+//  Created by Megan Miller on 8/10/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIView (TapAnimation)
+
+- (void)animateButtonWithCompletion:(void (^)(void))completion;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Soundroom/Resources/Extensions/UIView+TapAnimation.m
@@ -0,0 +1,43 @@
+//
+//  UIView+TapAnimation.m
+//  Soundroom
+//
+//  Created by Megan Miller on 8/10/22.
+//
+
+#import "UIView+TapAnimation.h"
+
+@implementation UIView (TapAnimation)
+
+- (void)animateButtonWithCompletion:(void (^)(void))completion {
+    
+    __weak UIView *weakSelf = self;
+    
+    self.userInteractionEnabled = NO;
+    
+    [UIView animateWithDuration:0.1
+                          delay:0
+                        options:UIViewAnimationOptionCurveLinear
+                     animations:^{ weakSelf.transform = CGAffineTransformMakeScale(0.95f, 0.95f); }
+                     completion:^(BOOL finished) {
+        
+                        if (finished) {
+                            [UIView animateWithDuration:0.1
+                                                  delay:0
+                                                options:UIViewAnimationOptionCurveLinear
+                                             animations:^{ weakSelf.transform = CGAffineTransformMakeScale(1.f, 1.f); }
+                                             completion:^(BOOL finished) {
+                                
+                                                if (finished) {
+                                                    self.userInteractionEnabled = YES;
+                                                    completion();
+                                                }
+                                
+                            }];
+                        }
+        
+    }];
+    
+}
+
+@end
